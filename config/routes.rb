1fa481ce1bbd84ffe1c2691d157083e2fb3e8206
Thanaphon Chaysawat <slumhumhlea@gmail.com>
2018-01-22 06:54:02 +0000

    bloccit_static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/contact'
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+  
+  root 'welcome#index'
+  # or using root({to: 'welcome#index'}) above we used is a implied hash syntax
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
